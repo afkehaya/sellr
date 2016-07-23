541b268c9728596ef68895eb32432905d5c9b2eb
Trevor Frese <trevor.frese@gmail.com>
2016-07-23 14:14:43 -0700

    Adds core attributes to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723211107) do
+ActiveRecord::Schema.define(version: 20160723211413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20160723211107) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "image"
+    t.string   "website"
+    t.string   "phone_number"
+    t.text     "bio"
+    t.string   "company_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
