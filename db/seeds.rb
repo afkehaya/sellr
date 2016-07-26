4b2f0cc54e81beeffb0a739f6abea7a8fc68ceeb
Kevin Malta <kevin.malta@gmail.com>
2016-07-25 23:07:51 -0700

    adds seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!([
+  {email: "kevin.malta@gmail.com", encrypted_password: "$2a$11$hH0w2oZR.YgHN5TYGwvUyOOhnoYpKjRAzXyYQVGOBe0xvHmL7h84q", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2016-07-26 05:27:48", last_sign_in_at: "2016-07-26 05:27:48", current_sign_in_ip: "::1", last_sign_in_ip: "::1", first_name: nil, last_name: nil, website: nil, phone_number: nil, bio: nil}
+])
+Listing.create!([
+  {commission: nil, value: 15.0, is_active: nil, requires_application: nil, description: "World's easist email marketing", is_sponsored: nil, product_url: "https://www.getresponse.com/partners", name: "GetResponse", image_file_name: "Get-Response-Logo-Light.jpeg", image_content_type: "image/jpeg", image_file_size: 24650, image_updated_at: "2016-07-26 05:28:15"},
+  {commission: nil, value: 19.0, is_active: nil, requires_application: nil, description: "Email is perfect for keeping your brand top of mind and staying connected with those who make a purchase, read your blog or meet you in person. And when you join the community where email automation began, you get access to all features and a team that’s 100% devoted to your success.", is_sponsored: nil, product_url: "http://www.aweber.com/affiliates.htm", name: "Aweber", image_file_name: "aweber.jpeg", image_content_type: "image/jpeg", image_file_size: 5721, image_updated_at: "2016-07-26 05:29:35"},
+  {commission: nil, value: 29.0, is_active: nil, requires_application: nil, description: "It is the email marketing software if you want more automation for less money.", is_sponsored: nil, product_url: "https://convertkit.com/partner/", name: "ConvertKit", image_file_name: "convertkit.jpeg", image_content_type: "image/jpeg", image_file_size: 80048, image_updated_at: "2016-07-26 05:31:01"},
+  {commission: nil, value: 49.0, is_active: nil, requires_application: nil, description: "Add a Q/A section on your website.", is_sponsored: nil, product_url: "https://answerbase.com/Affiliates.aspx", name: "AnswerBase", image_file_name: "AnswerbaseLogo.jpeg", image_content_type: "image/jpeg", image_file_size: 6857, image_updated_at: "2016-07-26 05:32:18"},
+  {commission: nil, value: 29.0, is_active: nil, requires_application: nil, description: "Snip.ly is a marketing service that allows you to add a message to any link that you share on the internet.", is_sponsored: nil, product_url: "http://snip.ly/affiliates/", name: "Snip.ly", image_file_name: "sniply-logo.jpeg", image_content_type: "image/jpeg", image_file_size: 16295, image_updated_at: "2016-07-26 05:34:28"},
+  {commission: nil, value: 29.0, is_active: nil, requires_application: nil, description: "Track organic and ad campaigns", is_sponsored: nil, product_url: "https://www.improvely.com/referral_program", name: "Improvely.com", image_file_name: "improvely.jpeg", image_content_type: "image/jpeg", image_file_size: 1901, image_updated_at: "2016-07-26 05:37:28"},
+  {commission: nil, value: 10.0, is_active: nil, requires_application: nil, description: "Hosting for developers", is_sponsored: nil, product_url: "www.webfaction.com/affiliate-program", name: "WebFaction", image_file_name: "webfaction.jpeg", image_content_type: "image/jpeg", image_file_size: 12259, image_updated_at: "2016-07-26 05:38:50"},
+  {commission: nil, value: 17.0, is_active: nil, requires_application: nil, description: "Premium quality hosting, storage, and computing, and has a dedicated version of WordPress called DreamPress", is_sponsored: nil, product_url: "https://www.dreamhost.com/affiliates/", name: "DreamHost", image_file_name: "dreamhost-logo.jpeg", image_content_type: "image/jpeg", image_file_size: 8374, image_updated_at: "2016-07-26 05:40:05"},
+  {commission: nil, value: 15.0, is_active: nil, requires_application: nil, description: "Managed WordPress hosting, business hosting, and dedicated servers.", is_sponsored: nil, product_url: "https://tdwebservices.com/affiliate/", name: "TD Web Services", image_file_name: "TDWebServices-small.jpeg", image_content_type: "image/jpeg", image_file_size: 11780, image_updated_at: "2016-07-26 05:41:15"},
+  {commission: nil, value: 90.0, is_active: nil, requires_application: nil, description: "Hire expert freelancers for your online job", is_sponsored: nil, product_url: "https://www.freelancer.com/affiliates/", name: "Freelancer.com", image_file_name: "freelancer.jpeg", image_content_type: "image/jpeg", image_file_size: 18014, image_updated_at: "2016-07-26 05:42:31"},
+  {commission: nil, value: 1.0, is_active: nil, requires_application: nil, description: "SeoClerks is a micro freelance marketplace where you get paid from small task", is_sponsored: nil, product_url: "https://www.seoclerks.com/affiliate", name: "SeoClerks.com", image_file_name: "seoclerks.jpeg", image_content_type: "image/jpeg", image_file_size: 22974, image_updated_at: "2016-07-26 05:43:58"},
+  {commission: nil, value: 70.0, is_active: nil, requires_application: nil, description: "This tool analyzes your website or any competitor website", is_sponsored: nil, product_url: "https://www.berush.com/", name: "SEMRush", image_file_name: "semrush.jpeg", image_content_type: "image/jpeg", image_file_size: 34643, image_updated_at: "2016-07-26 05:45:11"},
+  {commission: nil, value: 39.0, is_active: nil, requires_application: nil, description: "Inbound marketing services and SEO software to get more web traffic.", is_sponsored: nil, product_url: "http://upcity.com/partnership-program/", name: "Upcity", image_file_name: "upcity.jpeg", image_content_type: "image/jpeg", image_file_size: 10441, image_updated_at: "2016-07-26 05:49:45"},
+  {commission: nil, value: 25.0, is_active: nil, requires_application: nil, description: "Ashop allows merchants to start an online store easily.", is_sponsored: nil, product_url: "http://www.ashopcommerce.com/g/1060279/partner-program.html#1", name: "Ashop", image_file_name: "ashop.jpeg", image_content_type: "image/jpeg", image_file_size: 3609, image_updated_at: "2016-07-26 05:51:20"},
+  {commission: nil, value: 29.0, is_active: nil, requires_application: nil, description: "Create an online store for your product.", is_sponsored: nil, product_url: "https://www.shopify.com/affiliates", name: "Shopify", image_file_name: "shopify.jpeg", image_content_type: "image/jpeg", image_file_size: 76057, image_updated_at: "2016-07-26 05:52:33"},
+  {commission: nil, value: 10.0, is_active: nil, requires_application: nil, description: "Multiple social media account management tool.", is_sponsored: nil, product_url: "https://hootsuite.com/about/partner-programs/affiliate-program", name: "HootSuite", image_file_name: "hootsuite.jpeg", image_content_type: "image/jpeg", image_file_size: 97107, image_updated_at: "2016-07-26 05:53:43"},
+  {commission: nil, value: 18.0, is_active: nil, requires_application: nil, description: "SocialOomph.com is a service that provides free and paid productivity enhancement services for social media users.", is_sponsored: nil, product_url: "https://www.socialoomph.com/affiliates", name: "SocialOomph", image_file_name: "socialoomph.jpeg", image_content_type: "image/jpeg", image_file_size: 38723, image_updated_at: "2016-07-26 05:55:00"}
+])
+ListingsUser.create!([
+  {user_id: 1, listing_id: 1, role: "Merchant"},
+  {user_id: 1, listing_id: 2, role: "Merchant"},
+  {user_id: 1, listing_id: 3, role: "Merchant"},
+  {user_id: 1, listing_id: 4, role: "Merchant"},
+  {user_id: 1, listing_id: 5, role: "Merchant"},
+  {user_id: 1, listing_id: 6, role: "Merchant"},
+  {user_id: 1, listing_id: 7, role: "Merchant"},
+  {user_id: 1, listing_id: 8, role: "Merchant"},
+  {user_id: 1, listing_id: 9, role: "Merchant"},
+  {user_id: 1, listing_id: 10, role: "Merchant"},
+  {user_id: 1, listing_id: 11, role: "Merchant"},
+  {user_id: 1, listing_id: 12, role: "Merchant"},
+  {user_id: 1, listing_id: 13, role: "Merchant"},
+  {user_id: 1, listing_id: 14, role: "Merchant"},
+  {user_id: 1, listing_id: 15, role: "Merchant"},
+  {user_id: 1, listing_id: 16, role: "Merchant"},
+  {user_id: 1, listing_id: 17, role: "Merchant"}
+])
