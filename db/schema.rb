de4e6d9e91e0484c6230d09b370c8bd10713e0b9
Alexander Kehaya <afkehaya@gmail.com>
2016-07-26 08:37:35 -0700

    added simple merchant role to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725044056) do
+ActiveRecord::Schema.define(version: 20160726153435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,12 +43,37 @@ ActiveRecord::Schema.define(version: 20160725044056) do
     t.integer  "user_id"
     t.integer  "listing_id"
     t.string   "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "merchant_id"
   end
 
   add_index "listings_users", ["listing_id"], name: "index_listings_users_on_listing_id", using: :btree
   add_index "listings_users", ["user_id"], name: "index_listings_users_on_user_id", using: :btree
+
+  create_table "merchants", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "company"
+    t.string   "phone_number"
+    t.string   "website"
+    t.string   "bio"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "merchants", ["email"], name: "index_merchants_on_email", unique: true, using: :btree
+  add_index "merchants", ["reset_password_token"], name: "index_merchants_on_reset_password_token", unique: true, using: :btree
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
@@ -75,6 +100,7 @@ ActiveRecord::Schema.define(version: 20160725044056) do
     t.string   "phone_number"
     t.string   "bio"
     t.integer  "role_id"
+    t.boolean  "merchant"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
