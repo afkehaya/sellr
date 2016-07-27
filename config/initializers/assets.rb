63d4f2a67a4f375a0cc49c435eb6d035b5a0a041
Kevin Malta <kevin.malta@gmail.com>
2016-07-26 18:59:00 -0700

    adds styles to listing and edit listing page and adds javascript to remove the merchant button if user is a merchant

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( display_image.js )
+Rails.application.config.assets.precompile += %w( remove_merchant_button.js )
