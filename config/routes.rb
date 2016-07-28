778aab4363056c676ad250201c1a8124d4f883e5
Alexander Kehaya <afkehaya@gmail.com>
2016-07-28 11:48:55 -0700

    playing with routes and removing barrier to entry for authorizatoin.

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
   end
 
   authenticate :user do
-    resources :listings, only: [:new, :create, :edit, :update, :destroy, :show]
+    resources :listings, only: [:new, :create, :edit, :update, :destroy]
   end
-  post '/merchant_applications/new' => 'merchant_applications#new'
-  post '/thankyou' => 'merchant_applications#thankyou'
-  get '/thankyou' => 'merchant_applications#thankyou'
+  post '/merchant_applications/new', to: 'merchant_applications#new', as: "merchant"
+  post '/thankyou' => 'merchant_applications#thankyou', as: "thanks"
+  get '/thankyou' => 'merchant_applications#thankyou', as: "thankyou"
   post '/terms' => 'static#terms'
   get '/terms' => 'static#terms'
   post '/about' => 'static#about'
